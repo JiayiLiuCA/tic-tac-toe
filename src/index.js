9c87efd67dafee82eee078c19464af5e8fc43708
JiayiLiuCA <liujiayi402@gmail.com>
2020-04-06 23:15:57 -0400

    fix re-write and add winning logic, winner as a state to improve performance

--- src/index.js
+++ src/index.js
@@ -21,15 +21,20 @@ class Board extends React.Component {
     this.state = {
       squares : Array(9).fill(null),
       xIsNext : true,
+      winner : null,
     }
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice();
+    if (this.state.winner || squares[i]) {
+        return;
+    }
     squares[i] = this.state.xIsNext? 'X' : 'O';
     this.setState({
       squares:squares, 
-      xIsNext: !this.state.xIsNext
+      xIsNext: !this.state.xIsNext,
+      winner : calculateWinner(squares),
     });
   }
   // Add win Logic, remove overwrite
@@ -45,7 +50,14 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+    //const winner = calculateWinner(this.state.squares);
+    let status;
+    if (this.state.winner) {
+      status = 'winner is ' + this.state.winner + '!';
+    }
+    else {
+      status = 'Next player: ' + (this.state.xIsNext? 'X' : 'O');
+    }
 
     return (
       <div>
@@ -92,3 +104,23 @@ ReactDOM.render(
   <Game />,
   document.getElementById('root')
 );
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
+}
